d1d5ef71d708cc95cf707c225f9af4bf9220f2b2
Данила <117807019+Toxicmxtr@users.noreply.github.com>
2023-06-07 02:43:38 +0300

    Add files via upload

---
+++ 1.5.pas
@@ -0,0 +1,23 @@
+﻿var 
+a, b, c, h: integer;
+function j(m, n: integer): integer;
+var
+  g: integer;
+begin
+  g := m mod n;
+  if g = 0 then
+    j := n
+  else
+    j := j(n, g)
+end;
+begin
+  writeln('Введите два числа: ');
+  readln(a, b);
+  if a < b then begin
+    c := a;
+    a := b;
+    b := c;
+  end;
+  h := j(a, b);
+  writeln('НОД = ', h);
+end.
---
+++ 1.6.pas
@@ -0,0 +1,9 @@
+﻿procedure fib(c, i, n :integer);
+begin
+  if c=0 then Exit;
+  print(i);
+  fib(c-1, i+n, i);
+end;
+begin
+  fib(10,1,0);
+end.
